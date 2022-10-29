154b59879f40df425886ed93ec277b2c68cde58d
masa_81 <hope.is.a.waking.dream.8.1@gmail.com>
2022-10-29 20:31:06 +0900

    dbを修正

--- db/migrate/20221029111144_create_line_foods.rb
+++ db/migrate/20221029111144_create_line_foods.rb
@@ -5,8 +5,8 @@ class CreateLineFoods < ActiveRecord::Migration[6.0]
       t.references :food, null: false, foreign_key: true
       t.references :restaurant, null: false, foreign_key: true
       t.references :order, foreign_key: true
-      t.integer :count, null: false default:0
-      t.boolean :active, null: false default: false
+      t.integer :count, null: false, default: 0
+      t.boolean :active, null: false, default: false
 
       t.timestamps
     end
